7d832ab8332c6e52a310d769321bd1bc9dc8745e
Strangedip <sg02295@gmail.com>
2023-12-22 19:21:58 +0530

    added skill section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,5 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngProject';
-
-  @HostListener('window:scroll', ['$event'])
-  onScroll() {
-
-    // to move navbar up or down
-    const container = document.querySelector('.container');
-    const scrollPosition = window.scrollY || document.documentElement.scrollTop;
-    if (container) {
-      if (scrollPosition > 470) {
-        container?.classList.add('bottom');
-      } else {
-        container?.classList.remove('bottom');
-      }
-    }
-
-  }
+  title = 'Portfolio Project';
 }
