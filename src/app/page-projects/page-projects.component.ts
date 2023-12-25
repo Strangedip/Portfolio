07f0abdd35726849f5e2b05e11a549ab0a820c61
Strangedip <sg02295@gmail.com>
2023-12-26 04:35:17 +0530

    added project and contact section

--- src/app/page-projects/page-projects.component.ts
+++ src/app/page-projects/page-projects.component.ts
@@ -12,4 +12,23 @@ export class PageProjectsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  projects: Project[] = [
+    new Project("Proj1", "proj1 desc", "proj1link", "assets/img/prj.jpeg"),
+    new Project("Proj2", "proj2 desc", "proj2link", "assets/img/prj.jpeg"),
+    new Project("Proj3", "proj3 desc", "proj3link", "assets/img/prj.jpeg")];
 }
+
+class Project {
+  name: String;
+  desc: String;
+  link: String;
+  src: String;
+  constructor(name: String, desc: String, link: String, src: String) {
+    this.name = name;
+    this.desc = desc;
+    this.link = link;
+    this.src = src;
+  }
+}
+
+
