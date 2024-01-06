a4a7f47a2397538e03612d4cb8b1537d3385511e
Strangedip <sg02295@gmail.com>
2024-01-06 20:26:45 +0530

    minor fix and responsive till 750px

--- src/app/float-navbar/float-navbar.component.ts
+++ src/app/float-navbar/float-navbar.component.ts
@@ -25,7 +25,7 @@ export class FloatNavbarComponent implements OnInit {
     const navbar = document.querySelector('.navbar');
     const scrollPosition = window.scrollY || document.documentElement.scrollTop;
     if (navContainer) {
-      if (scrollPosition > 470) {
+      if (scrollPosition > window.innerHeight / 2.5) {
         navContainer.classList.add('scrolled');
         navbar?.classList.add('bottom');
       } else {
@@ -38,8 +38,10 @@ export class FloatNavbarComponent implements OnInit {
     const sections = document.querySelectorAll('section');
     sections.forEach(section => {
       const top = section.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+      const bottom = section.getBoundingClientRect().bottom;
       const id = section.getAttribute('id');
-      if (top < 425 && top > -425) {
+      if (top >= (0 - (windowHeight * (65 / 100))) && top < (windowHeight * (35 / 100))) {
         const element = document.querySelector(`a[id="${id}-"]`);
         if (element) {
           element.classList.add('active');
